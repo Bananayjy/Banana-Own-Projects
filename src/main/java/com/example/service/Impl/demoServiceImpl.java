5c05326da133c0852de5cc5646a5e7b468f8e8a5
yjy <2322092442@qq.com>
2023-12-05 20:41:01 +0800

    logic: init

--- src/main/java/com/example/service/Impl/demoServiceImpl.java
+++ src/main/java/com/example/service/Impl/demoServiceImpl.java
@@ -1,5 +1,8 @@
 package com.example.service.Impl;
 
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.example.mapper.DemoMapper;
+import com.example.model.entity.Demo;
 import com.example.service.demoService;
 import org.springframework.stereotype.Service;
 
@@ -8,7 +11,7 @@ import org.springframework.stereotype.Service;
  * @create 2023-12-05 18:52
  */
 @Service
-public class demoServiceImpl implements demoService {
+public class demoServiceImpl extends ServiceImpl<DemoMapper, Demo> implements demoService {
 
 
 }
