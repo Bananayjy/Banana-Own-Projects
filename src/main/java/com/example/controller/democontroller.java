d8589ad383853f50a7d16e2f2d09013ce02c5260
yjy <2322092442@qq.com>
2023-12-05 20:03:15 +0800

    logic: init

---
+++ src/main/java/com/example/controller/democontroller.java
@@ -0,0 +1,18 @@
+package com.example.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author banana
+ * @create 2023-10-06 21:57
+ */
+@RestController
+@RequestMapping("/demo")
+public class democontroller {
+
+
+
+}
---
+++ src/main/java/com/example/service/demoService.java
@@ -0,0 +1,9 @@
+package com.example.service;
+
+/**
+ * @author banana
+ * @create 2023-12-05 18:51
+ */
+public interface demoService {
+
+}
