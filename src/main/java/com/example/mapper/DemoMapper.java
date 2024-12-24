7522a8874e28ec64e37a9b8ad300bbfb921eb497
banana <2322092442@qq.com>
2024-12-24 18:54:20 +0800

    logic: mybatis init

--- src/main/java/com/example/mapper/DemoMapper.java
+++ src/main/java/com/example/mapper/DemoMapper.java
@@ -1,14 +1,8 @@
 package com.example.mapper;
 
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.example.model.entity.Demo;
-import org.apache.ibatis.annotations.Mapper;
 
-/**
- * @author banana
- * @create 2023-12-05 20:30
- */
-@Mapper
-public interface DemoMapper extends BaseMapper<Demo> {
+public interface DemoMapper {
 
+    Demo getById(Integer i);
 }
