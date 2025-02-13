74df0bef5e3fe04775924c9dab7db719277db4dc
banana <2322092442@qq.com>
2025-02-13 09:07:48 +0800

    feat: redis依赖引入

--- src/main/java/com/example/controller/democontroller.java
+++ src/main/java/com/example/controller/democontroller.java
@@ -1,9 +1,14 @@
 package com.example.controller;
 
 
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.Resource;
+import java.util.List;
 
 /**
  * @author banana
@@ -13,5 +18,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/demo")
 public class democontroller {
 
+    @Resource
+    private RedisTemplate redisTemplate;
+
+    @PostConstruct
+    public void init() {
+        System.out.println(redisTemplate);
+        redisTemplate.opsForValue().set("name2", "张三");
+    }
 
 }
